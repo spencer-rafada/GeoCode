c3eeb1a11efcd4636fcf57770e05167f61822669
Spencer Rafada <spencer.byui@gmail.com>
2022-11-08 15:30:55 -0700

    Initial Commit

---
+++ GeoCode/GeoCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoCodeApp.swift
+//  GeoCode
+//
+//  Created by Spencer Rafada on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct GeoCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
